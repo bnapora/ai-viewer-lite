43756a62802ebf615c55c53af2e1872c2b1a4715
melinda <melinda@melindaminch.com>
2021-12-16 14:33:36 -0800

    Step 1

--- js/utils/magnifier.js
+++ js/utils/magnifier.js
@@ -9,9 +9,9 @@
     //At some browser magnification levels the display regions lines up correctly, but at some there appears to
     //be a one pixel gap.
     const fudge = new $.Point(1, 1);
-    const checkboxId = 'magnifier__show-in-viewer';
-    const activeClass = 'magnifier--active';
-    const inactiveClass = 'magnifier--inactive';
+    const checkboxId = "magnifier__show-in-viewer";
+    const activeClass = "magnifier--active";
+    const inactiveClass = "magnifier--inactive";
 
     class Magnifier {
         constructor(mainViewer, options) {
@@ -58,8 +58,9 @@
 
             this.inViewerElement = $.makeNeutralElement("div");
             this.inViewerElement.id = this.element.id + "--inline";
-            this.inViewerElement.className = 'magnifier magnifier--square magnifier--inline'
-            if(this.showInViewer) {
+            this.inViewerElement.className =
+                "magnifier magnifier--square magnifier--inline";
+            if (this.showInViewer) {
                 $.addClass(this.inViewerElement, activeClass);
             } else {
                 $.addClass(this.inViewerElement, inactiveClass);
@@ -79,10 +80,13 @@
             ).minus(fudge);
 
             this.viewer = $(options);
-            // The same thing again, but inside the viewer instead of outside. It looks better twice as big.
-            this.inlineViewer = $($.extend(
-                options,
-                { element: this.inViewerElement, defaultZoomLevel: options.defaultZoomLevel })
+            // The same thing again, but inside the viewer instead of outside.
+            this.inlineViewer = $(
+                $.extend(options, {
+                    element: this.inViewerElement,
+                    // prevent resizing the magnifier from adding unwanted space around the image
+                    minZoomImageRatio: 1.0,
+                })
             );
 
             (function (style, borderWidth) {
@@ -116,9 +120,11 @@
                 self.update();
             });
 
-            document.getElementById(checkboxId).addEventListener('change', function() {
-                self.toggleInViewer();
-            })
+            document
+                .getElementById(checkboxId)
+                .addEventListener("change", function () {
+                    self.toggleInViewer();
+                });
 
             this.storedBounds = null;
             this.update();
@@ -126,92 +132,110 @@
 
         update() {
             const viewerSize = $.getElementSize(this.viewer.element);
-            const inlineViewerSize = $.getElementSize(this.inlineViewer.element);
+            const inlineViewerSize = $.getElementSize(
+                this.inlineViewer.element
+            );
 
             if (
                 this.mainViewer.viewport &&
                 this.viewer.viewport &&
-                this.inlineViewer.viewport) {
-                    const zoomTarget = this.mainViewer.viewport.getZoom() * this.ratio;
+                this.inlineViewer.viewport
+            ) {
+                // things we always want to do
+                const zoomTarget =
+                    this.mainViewer.viewport.getZoom() * this.ratio;
 
-                    this.viewer.viewport.zoomTo(zoomTarget);
-                    this.inlineViewer.viewport.zoomTo(zoomTarget * 2);
+                this.viewer.viewport.zoomTo(zoomTarget);
+                this.inlineViewer.viewport.zoomTo(zoomTarget);
 
-                    const center = this.mainViewer.viewport.getCenter();
-                    this.viewer.viewport.panTo(center);
-                    this.inlineViewer.viewport.panTo(center);
+                const center = this.mainViewer.viewport.getCenter();
+                this.viewer.viewport.panTo(center);
+                this.inlineViewer.viewport.panTo(center);
 
+                if (this.showInViewer) {
+                    // Event handing for when the inline magnifier is active
+                    var bounds = this.inlineViewer.viewport.getBounds(true);
+                } else {
+                    // Event handling for when the sidebar magnifier is active
                     var bounds = this.viewer.viewport.getBounds(true);
-                    var topleft = this.mainViewer.viewport.pixelFromPoint(
+                }
+
+                var topleft = this.mainViewer.viewport.pixelFromPoint(
                         bounds.getTopLeft(),
                         true
                     );
-                    var bottomright = this.mainViewer.viewport
-                        .pixelFromPoint(bounds.getBottomRight(), true)
-                        .minus(this.totalBorderWidths);
 
-                    //update style for magnifier-box
-                    var style = this.displayRegion.style;
-                    style.display = this.viewer.world.getItemCount()
-                        ? "block"
-                        : "none";
+                var bottomright = this.mainViewer.viewport
+                    .pixelFromPoint(bounds.getBottomRight(), true)
+                    .minus(this.totalBorderWidths);
 
-                    style.top = Math.round(topleft.y) + "px";
-                    style.left = Math.round(topleft.x) + "px";
+                //update style for magnifier-box
+                var style = this.displayRegion.style;
+                style.display = this.viewer.world.getItemCount()
+                    ? "block"
+                    : "none";
 
-                    var width = Math.abs(topleft.x - bottomright.x);
-                    var height = Math.abs(topleft.y - bottomright.y);
-                    // make sure width and height are non-negative so IE doesn't throw
-                    style.width = Math.round(Math.max(width, 0)) + "px";
-                    style.height = Math.round(Math.max(height, 0)) + "px";
+                style.top = Math.round(topleft.y) + "px";
+                style.left = Math.round(topleft.x) + "px";
 
-                    this.storedBounds = bounds;
+                var width = Math.abs(topleft.x - bottomright.x);
+                var height = Math.abs(topleft.y - bottomright.y);
+                // make sure width and height are non-negative so IE doesn't throw
+                style.width = Math.round(Math.max(width, 0)) + "px";
+                style.height = Math.round(Math.max(height, 0)) + "px";
 
+                this.storedBounds = bounds;
             }
         }
 
         toggleInViewer() {
-            if(this.showInViewer) {
+            if (this.showInViewer) {
                 this.showInViewer = false;
                 $.removeClass(this.inViewerElement, activeClass);
                 $.addClass(this.inViewerElement, inactiveClass);
                 this.inlineViewer.setVisible(false);
+                $.removeClass(this.element, inactiveClass);
+                $.addClass(this.element, activeClass);
             } else {
                 this.showInViewer = true;
                 $.removeClass(this.inViewerElement, inactiveClass);
                 $.addClass(this.inViewerElement, activeClass);
                 this.inlineViewer.setVisible(true);
                 this.inlineViewer.forceRedraw();
+                $.removeClass(this.element, activeClass);
+                $.addClass(this.element, inactiveClass);
             }
         }
 
         _createSuccessCallback(i, viewer) {
-            return function(i) {
+            return function (i) {
                 const layer0X = viewer.world.getItemAt(0).getContentSize().x;
-                const layerNX = viewer.world.getItemAt(viewer.world.getItemCount() - 1).getContentSize().x;
-                viewer.world.getItemAt(viewer.world.getItemCount() -1 ).setWidth(layerNX / layer0X);
-            }
+                const layerNX = viewer.world
+                    .getItemAt(viewer.world.getItemCount() - 1)
+                    .getContentSize().x;
+                viewer.world
+                    .getItemAt(viewer.world.getItemCount() - 1)
+                    .setWidth(layerNX / layer0X);
+            };
         }
 
         addLayer(url_suffix, opacity, layerName, tileSource, i) {
-            url_suffix = url_suffix ? url_suffix : '';
+            url_suffix = url_suffix ? url_suffix : "";
 
             this.viewer.addTiledImage({
                 index: i + 1,
                 tileSource: url_suffix + tileSource,
                 opacity: opacity,
-                success: this._createSuccessCallback(i, this.viewer)
+                success: this._createSuccessCallback(i, this.viewer),
             });
 
             this.inlineViewer.addTiledImage({
                 index: i + 1,
                 tileSource: url_suffix + tileSource,
                 opacity: opacity,
-                success: this._createSuccessCallback(i, this.inlineViewer)
+                success: this._createSuccessCallback(i, this.inlineViewer),
             });
         }
-
-
     }
     window.Magnifier = Magnifier;
 })(OpenSeadragon);
