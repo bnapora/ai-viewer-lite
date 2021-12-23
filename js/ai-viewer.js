38c4ee578ced1ec7c43a1a97249f480b39354750
melinda <melinda@melindaminch.com>
2021-12-22 16:50:29 -0800

    Marker ratios

--- js/ai-viewer.js
+++ js/ai-viewer.js
@@ -201,7 +201,7 @@ tmapp.init = function () {
         // todo: should I make GL an attribute on each OSD viewer instead?
         tmapp['viewerGl'] = new glUtils(tmapp[vname]);
         tmapp['magGl'] = new glUtils(tmapp[mname].viewer, 0.5);
-        tmapp['inlineMagGl'] = new glUtils(tmapp[mname].inlineViewer, 0.25);
+        tmapp['inlineMagGl'] = new glUtils(tmapp[mname].inlineViewer, 0.5);
 
     } else {
         console.log("Using CPU-based marker drawing (SVG canvas)")
